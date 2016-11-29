cba0048dd12a81c0fa6e208e2959c58bb99175db
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-11-29 16:54:35 +0300

    add state component

--- examples/firstSteps/app.jsx
+++ examples/firstSteps/app.jsx
@@ -1,23 +1,34 @@
 var str = "String from variable";
 
-function Hero(props) {
-  return (
-    <div className="container">
-      <img src={props.imgUrl} alt="React logo" className="main-logo" />
-      <h1>{props.title}</h1>
-      <p>{props.text}</p>
-      <p>{str}</p>
-    </div>
-  );
-}
+var Hero = React.createClass({
+  getInitialState: function() {
+    return {
+      count: 0
+    };
+  },
+  handleClick: function() {
+    this.setState({ count: this.state.count + 1 });
+  },
+  render: function() {
+    return (
+      <div className="container">
+        <div>{this.state.count}</div>
+        <img src={this.props.imgUrl} alt="React logo" className="main-logo" onClick={this.handleClick} />
+        <h1>{this.props.title}</h1>
+        <p>{this.props.text}</p>
+        <p>{str}</p>
+      </div>
+    )
+  }
+});
 
 ReactDOM.render(
             <div>
               <Hero title="Hello React"
                     text="React is cool view library"
-                    imgUrl="https://facebook.github.io/react/img/logo.svg"/>
+                    imgUrl="https://facebook.github.io/react/img/logo.svg" />
               <Hero title="Hello Angular"
                     text="Angular is framework"
-                    imgUrl="https://angular.io/resources/images/logos/angular2/angular.svg"/>
+                    imgUrl="https://angular.io/resources/images/logos/angular2/angular.svg" />
             </div>,
   document.getElementById('root'));
