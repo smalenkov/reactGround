7d8b66a301da09f0c047d00b2c2f068752dd919a
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-11-29 11:31:07 +0300

    add jsx syntax

--- examples/firstSteps/app.js
+++ examples/firstSteps/app.js
@@ -1,3 +1,13 @@
-var element = React.createElement('h1', null, 'Hello React');
+var logo = React.createElement('img', {
+  src: 'https://facebook.github.io/react/img/logo.svg',
+  className: 'main-logo'
+});
 
-ReactDOM.render(element, document.getElementById('root'));
+var title = React.createElement('h1', null, 'Hello React');
+var text = React.createElement('p', null, 'React is cool view library');
+
+var container = React.createElement('div', {
+    className: 'container'
+  }, logo, title, text);
+
+ReactDOM.render(container, document.getElementById('root'));
---
+++ examples/firstSteps/app.jsx
@@ -0,0 +1,8 @@
+var jsxContainer =
+  <div className="container">
+    <img src="https://facebook.github.io/react/img/logo.svg" alt="React logo" className="main-logo" />
+    <h1>Hello React</h1>
+    <p>React is cool view library</p>
+  </div>;
+
+ReactDOM.render(jsxContainer, document.getElementById('root'));
