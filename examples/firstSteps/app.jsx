477356e680b04d5eeae539fded0a3aedeb862624
Vladimir <smalenkoff@gmail.com>
2016-11-29 22:31:47 +0300

    add state two in component

--- examples/firstSteps/app.jsx
+++ examples/firstSteps/app.jsx
@@ -3,17 +3,26 @@ var str = "String from variable";
 var Hero = React.createClass({
   getInitialState: function() {
     return {
-      count: 0
+      count: 0,
+      countTwo: 0
     };
   },
   handleClick: function() {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({
+      count: this.state.count + 1
+    });
+  },
+  handleMouseOver: function() {
+    this.setState({
+      countTwo: this.state.countTwo + 2
+    })
   },
   render: function() {
     return (
       <div className="container">
         <div>{this.state.count}</div>
-        <img src={this.props.imgUrl} alt="React logo" className="main-logo" onClick={this.handleClick} />
+        <div>{this.state.countTwo}</div>
+        <img src={this.props.imgUrl} alt="React logo" className="main-logo" onClick={this.handleClick} onMouseOver={this.handleMouseOver} />
         <h1>{this.props.title}</h1>
         <p>{this.props.text}</p>
         <p>{str}</p>
