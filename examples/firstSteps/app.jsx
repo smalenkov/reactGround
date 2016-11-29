a05ada5ff3576de49d07439981a0e64088d67cc4
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-11-29 14:05:23 +0300

    add functional component

--- examples/firstSteps/app.jsx
+++ examples/firstSteps/app.jsx
@@ -1,8 +1,23 @@
-var jsxContainer =
-  <div className="container">
-    <img src="https://facebook.github.io/react/img/logo.svg" alt="React logo" className="main-logo" />
-    <h1>Hello React</h1>
-    <p>React is cool view library</p>
-  </div>;
+var str = "String from variable";
 
-ReactDOM.render(jsxContainer, document.getElementById('root'));
+function Hero(props) {
+  return (
+    <div className="container">
+      <img src={props.imgUrl} alt="React logo" className="main-logo" />
+      <h1>{props.title}</h1>
+      <p>{props.text}</p>
+      <p>{str}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+            <div>
+              <Hero title="Hello React"
+                    text="React is cool view library"
+                    imgUrl="https://facebook.github.io/react/img/logo.svg"/>
+              <Hero title="Hello Angular"
+                    text="Angular is framework"
+                    imgUrl="https://angular.io/resources/images/logos/angular2/angular.svg"/>
+            </div>,
+  document.getElementById('root'));
