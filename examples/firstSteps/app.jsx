f091627a226fe5643b5dae5d5ff1a4d34ae1f9a9
Vladimir <smalenkoff@gmail.com>
2016-11-30 22:29:13 +0300

    data show

--- examples/firstSteps/app.jsx
+++ examples/firstSteps/app.jsx
@@ -1,4 +1,36 @@
-var Hero = React.createClass({
+var data = [{
+              name: "Petr",
+              lastname: "Semenov",
+              age: 23
+            },
+            {
+              name: "Vladimir",
+              lastname: "Nikolaev",
+              age: 18
+            },
+            {
+              name: "Igor",
+              lastname: "Grishin",
+              age: 25
+            },
+            {
+              name: "Semen",
+              lastname: "Ivanov",
+              age: 25
+            },
+            {
+              name: "Vladimir",
+              lastname: "Goncharov",
+              age: 20
+            }];
+
+const title = 'Click frontend favorites';
+var element = <h1 className="main-title">{title}</h1>;
+
+
+// Classes
+
+var ElemBox = React.createClass({
   getInitialState: function() {
     return {
       count: 0,
@@ -28,18 +60,40 @@ var Hero = React.createClass({
   }
 });
 
-const title = 'Click frontend favorites';
-var element = <h1 className="main-title">{title}</h1>;
+var Heroes = React.createClass({
+  render: function() {
+    return (
+          <div>
+        {
+          this.props.data.map(function(item) {
+            return (
+                <div className="heroes-container">
+                  <div>{item.name}</div>
+                  <div>{item.lastname}</div>
+                  <div className="heroes-age">{item.age}</div>
+                </div>
+                )
+          })
+        }
+          </div>
+    )
+  }
+});
+
+
+// Renders
 
 ReactDOM.render(
             <div>
-              <Hero title="Hello React"
-                    text="React is cool view library"
-                    imgUrl="https://facebook.github.io/react/img/logo.svg" />
-              <Hero title="Hello Angular"
-                    text="Angular is framework"
-                    imgUrl="https://angular.io/resources/images/logos/angular2/angular.svg" />
+              <ElemBox title="Hello React"
+                       text="React is cool view library"
+                       imgUrl="https://facebook.github.io/react/img/logo.svg" />
+              <ElemBox title="Hello Angular"
+                       text="Angular is framework"
+                       imgUrl="https://angular.io/resources/images/logos/angular2/angular.svg" />
             </div>,
   document.getElementById('root'));
 
 ReactDOM.render(element, document.getElementById('mainTitle'));
+
+ReactDOM.render(<Heroes data={data} />, document.getElementById('heroes'));
